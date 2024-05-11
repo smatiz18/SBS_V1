89498f889791adebebe4cac7ddc4aa835413a9fb
smatiz18 <s.matiz00@gmail.com>
2024-05-11 11:12:52 -0400

    commit from mb

--- sbs-v1-ui/src/pages/nba/nba.tsx
+++ sbs-v1-ui/src/pages/nba/nba.tsx
@@ -1,7 +1,16 @@
 const NBA = () => {
     return (
         <div className="nba-container">
-        </div>
+            <div className="nba-header">
+                <h3>NBA Games Today</h3>
+            </div>
+            <div className="nba-body">
+
+            </div>
+            <div className="nba-footer">
+
+            </div>
+       </div>
     );
 }
 
