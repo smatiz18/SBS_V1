424b59411869cffa09d7f392416a39ce70cc7b70
smatiz18 <s.matiz00@gmail.com>
2024-08-02 11:08:40 -0400

    adding initial rust main.rs... need to setup correct config

--- sbs-v1-rust-backend/rust_backend/src/main.rs
+++ sbs-v1-rust-backend/rust_backend/src/main.rs
@@ -1,3 +1,50 @@
-fn main() {
-    println!("Hello, world!");
+use mongodb::{Client, options::{ClientOptions, ResolverConfig}};
+use std::env;
+use actix_web::{get, post, web, App, HttpResponse, HttpServer, Responder};
+
+#[get("/")]
+async fn hello() -> impl Responder {
+    HttpResponse::Ok().body("Hello world!")
+}
+
+#[post("/echo")]
+async fn echo(req_body: String) -> impl Responder {
+    HttpResponse::Ok().body(req_body)
+}
+
+async fn manual_hello() -> impl Responder {
+    HttpResponse::Ok().body("Hey there!")
+}
+
+#[actix_web::main]
+async fn main() -> Result<(), Box<dyn std::error::Error>> {
+   // Load the MongoDB connection string from an environment variable:
+   let client_uri =
+      env::var("SBS_V1_MONGO_URI").expect("You must set the SBS_V1_MONGO_URI environment var!");
+
+   // A Client is needed to connect to MongoDB:
+   // An extra line of code to work around a DNS issue on Windows:
+   let options =
+      ClientOptions::parse_with_resolver_config(&client_uri, ResolverConfig::cloudflare())
+         .await?;
+   
+   let client = Client::with_options(options)?;
+
+   // Print the databases in our MongoDB cluster:
+   println!("Databases:");
+   for name in client.list_database_names(None, None).await? {
+      println!("- {}", name);
+   }
+
+   HttpServer::new(|| {
+        App::new()
+            .service(hello)
+            .service(echo)
+            .route("/hey", web::get().to(manual_hello))
+    })
+    .bind(("127.0.0.1", 8080))?
+    .run()
+    .await?;
+
+   Ok(())
 }
