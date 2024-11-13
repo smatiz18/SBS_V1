aa5f44e1fd6b796967a0a6d976874ddded9893a4
smatiz18 <s.matiz00@gmail.com>
2024-11-13 03:06:24 -0500

    starting to configure data nba loader runner in rust

--- sbs-v1-rust-backend/rust_backend/src/bin/daily_data_loaders.rs
+++ sbs-v1-rust-backend/rust_backend/src/bin/daily_data_loaders.rs
@@ -1,3 +1,5 @@
-fn main() {
+use rust_backend::data_loaders::nba::daily_loaders::daily_nba_game_data_loader;
 
+fn main() {
+    daily_nba_game_data_loader();
 }
--- sbs-v1-rust-backend/rust_backend/src/data_loaders/nba/daily_loaders.rs
+++ sbs-v1-rust-backend/rust_backend/src/data_loaders/nba/daily_loaders.rs
@@ -1,29 +1,48 @@
 #![allow(dead_code)]
+
+use std::fs;
+
+use pyo3::prelude::*;
+use pyo3::types::PyTuple;
+
 use crate::models::app_state::AppState;
+
 /** Old loader logic that is already written in python will stay that way. In the future 
  * we should migrate this over to rust but for the sake of prototyping let's keep it in python
  */
 
-async fn load_latest_nba_game_stats() {
-    // let python_code = fs::read_to_string().expect("Failed to read Python file");
+pub fn daily_nba_game_data_loader() -> PyResult<()> {
+    let file_name = "nba_mongo_loaders.py";
+    let module_name = "sbs-v1-python-backend";
+    let path = format!("../../{}/app/scripts/{}", module_name, file_name);
+    
+    let source_code = match fs::read_to_string(path) {
+        Ok(code) => code,
+        Err(e) => { 
+            print!("failed to open filed at path: {:?}", e);
+            return Ok(());
+        }
+    };
+
+    Python::with_gil(|py| {
+        let loader_source_code: Py<PyAny> = PyModule::from_code_bound(
+            py,
+            &source_code,
+            &file_name,
+            &module_name
+        )?
+        .into();
+
+
+        match loader_source_code.call_method0(py, "run_nba_daily_games_data_loader")  {
+            Ok(res) => print!("Success in running run_nba_daily_games_data_loader: {:?}", res),
+            Err(e) => print!("Failed to run run_nba_daily_games_data_loader: {:?}", e)
+        }
+        
+        Ok(())
+    })
 }
 
-async fn load_latest_nba_player_stats(_app_state: AppState) {
-
-}
-
-async fn load_latest_nba_team_stats(_app_state: AppState) {
-
-}
-
-async fn load_historical_nba_game_stats(_app_state: AppState) {
-
-}
-
-async fn load_historical_nba_player_stats(_app_state: AppState) {
-
-}
-
-async fn load_historical_nba_team_stats(_app_state: AppState) {
+pub async fn daily_nba_player_game_data_loader(_app_state: AppState) {
 
 }
